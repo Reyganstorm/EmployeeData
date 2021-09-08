d4b989e73668e302c4cb94457b27cefab1a42306
Ruslan Shtybaev <ruslan.shtybaev@gmail.com>
2021-09-08 20:51:02 +0300

    Initial Commit

---
+++ EmployeeData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmployeeData
+//
+//  Created by Руслан Штыбаев on 08.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
